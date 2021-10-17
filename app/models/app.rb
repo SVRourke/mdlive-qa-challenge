8fe452326c6c1346093d2528654715f518b76333
Samuel Rourke <samuelvanrourke@gmail.com>
2021-10-17 16:17:03 -0400

    created a static method to simplify the query

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,2 +1,6 @@
 class App < ApplicationRecord
+  def self.simpleQ(params)
+    by, offset, limit, order = params.values_at(:by, :offset, :limit, :order)
+    return App.order("#{by} #{order}").limit(limit).offset(offset)
+  end
 end
